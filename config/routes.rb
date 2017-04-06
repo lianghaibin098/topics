291c23fa9bd17a1a4d967d9dd57ff5a49e46933d
lianghaibin <lianghaibin1688@gmail.com>
2017-04-07 06:59:46 +0800

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+
   root "topics#index"
   resources :topics do
     member do
       post "upvote"
+      post "downvote"
     end
   end
 end
